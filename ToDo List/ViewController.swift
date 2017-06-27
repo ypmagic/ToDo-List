3a69ba2f95476ab65c6bac0a788887a26c1f2c5a
Young Park <young_park@brown.edu>
2017-06-27 10:57:39 +0900

    Initial Commit

---
+++ ToDo List/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ToDo List
+//
+//  Created by Young Park on 6/27/17.
+//  Copyright © 2017 Young Park. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
